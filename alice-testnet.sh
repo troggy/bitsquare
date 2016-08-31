bc8c6480ae9ce4a35f4947a8477149ff225a7737
Kosta Korenkov <kosta@whoot.me>
2016-08-31 19:04:00 +0300

    Scripts to launch Alice and Bob test users on TESTNET

---
+++ alice-testnet.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar gui/target/shaded.jar --nodePort=3332 --bitcoinNetwork=TESTNET --appName=Bitsquare-Local-Testnet-Alice
---
+++ bob-testnet.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar gui/target/shaded.jar --bitcoinNetwork=TESTNET --nodePort=4332 --appName=Bitsquare-Local-Testnet-Bob
